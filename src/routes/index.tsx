d559fb19105d567c9e0d72dafcf885773b00ff2b
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-13 15:59:41 +0500

    !feat(Router): added router

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Routes: FC<{}> = () => (
+  <>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={} />
+      </Switch>
+    </Router>
+  </>
+)
+
+export default Routes;
