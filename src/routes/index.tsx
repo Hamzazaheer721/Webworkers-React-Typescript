6ffe2d8069cee1b5cdfa90749cc6c58a572ff2cb
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-13 17:59:08 +0500

    !fix(CT-1): Routes have been added to router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
 import { FC, memo } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Loop from './Loop';
+import Multiply from './Multiply';
 import Welcome from './Welcome';
 
 const RoutesComponent: FC<{}> = memo(() => (
   <Router>
     <Switch>
       <Route exact path="/" component={Welcome} />
+      <Route exact path="/loop" component={Loop} />
+      <Route exact path="/multiply" component={Multiply} />
     </Switch>
   </Router>
 ));
