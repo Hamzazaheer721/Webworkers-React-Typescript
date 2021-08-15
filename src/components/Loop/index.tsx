5c7e0d059d843076720f80ca29bc62f7dcb5a9cd
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-16 00:24:55 +0500

    !fix(CT-3): states added

--- src/components/Loop/index.tsx
+++ src/components/Loop/index.tsx
@@ -1,27 +1,33 @@
-import { FC, memo } from 'react';
+/* eslint-disable no-unused-vars */
+import { FC, memo, useState } from 'react';
 import {
   Button, ButtonContainer, Container, CountContainer, Heading, StateHeading,
 } from './index.styled';
 
-const LoopComponent : FC = memo(() => (
-  <Container>
-    <ButtonContainer>
-      <Button>
-        Tomato
-      </Button>
-      <Button>
-        Apple
-      </Button>
-    </ButtonContainer>
-    <CountContainer>
-      <Heading>Tomato</Heading>
-      <StateHeading>1</StateHeading>
-    </CountContainer>
-    <CountContainer>
-      <Heading>Apple</Heading>
-      <StateHeading>1</StateHeading>
-    </CountContainer>
-  </Container>
-));
+const LoopComponent : FC = memo(() => {
+  const [tomatoCount, setTomatoCount] = useState<number>(0)
+  const [appleCount, setAppleCount] = useState<number>(0);
+
+  return (
+    <Container>
+      <ButtonContainer>
+        <Button>
+          Tomato
+        </Button>
+        <Button>
+          Apple
+        </Button>
+      </ButtonContainer>
+      <CountContainer>
+        <Heading>Tomato</Heading>
+        <StateHeading>{tomatoCount}</StateHeading>
+      </CountContainer>
+      <CountContainer>
+        <Heading>Apple</Heading>
+        <StateHeading>{appleCount}</StateHeading>
+      </CountContainer>
+    </Container>
+  )
+});
 
 export default LoopComponent;
