38fa1fe9f941b5323866a54a49a30ca64ba3a109
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-24 09:43:50 +0500

    !fix(CT-8): json added

--- src/components/Virtualization/index.tsx
+++ src/components/Virtualization/index.tsx
@@ -1,7 +1,34 @@
-import { FC, memo } from 'react';
+/* eslint-disable no-console */
+import {
+  useEffect, FC, memo, useState,
+} from 'react';
+import Data from '../../assets/json/generated.json'
 
-const VirtualizedComponent: FC = memo(() => (
-  <h1>Virtualized Component</h1>
-));
+interface IDataProps {
+    phone: string,
+    address: string,
+    _id: string,
+    guid: string,
+    picture: string,
+    age : number,
+    name: string,
+    gender: string,
+    company: string,
+    email: string
+
+}
+
+const VirtualizedComponent: FC = memo(() => {
+  const [data] = useState<IDataProps[]>(Data)
+
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.info('data', data)
+  }, [data])
+
+  return (
+    <h1>Virtualized Component</h1>
+  )
+});
 
 export default VirtualizedComponent;
