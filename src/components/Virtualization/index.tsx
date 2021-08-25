5c8e8c81f28190d451b00856ea6ee85fc1cce94d
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-25 12:34:45 +0500

    !feat(CT-8): added ant-design table

--- src/components/Virtualization/index.tsx
+++ src/components/Virtualization/index.tsx
@@ -6,21 +6,33 @@ import Data from '../../assets/json/generated.json'
 import IDataType from '../../helper/types';
 import InputComponent from '../InputField';
 import TableComponent from '../Table';
+import VirtualTable from '../VirtualTable';
 import { Heading, Wrapper } from './index.styled';
 
 const VirtualizedComponent: FC = memo(() => {
   const [data] = useState<IDataType[]>(Data)
   const [value, setValue] = useState<string>('');
 
-  const changeValue = useCallback((_val : string) => {
+  const changeValue = useCallback((_val: string) => {
     setValue(_val)
   }, [])
+  // Usage
+  const columns = [
+    { title: 'A', dataIndex: 'key', width: 150 },
+    { title: 'B', dataIndex: 'key' },
+    { title: 'C', dataIndex: 'key' },
+    { title: 'D', dataIndex: 'key' },
+    { title: 'E', dataIndex: 'key', width: 200 },
+    { title: 'F', dataIndex: 'key', width: 100 },
+  ];
 
+  const virtualData = Array.from({ length: 100000 }, (_, key) => ({ key }));
   return (
     <Wrapper>
       <Heading>Virtualized Component</Heading>
       <InputComponent changeValue={changeValue} />
       <TableComponent data={data} value={value} />
+      <VirtualTable columns={columns} dataSource={virtualData} scroll={{ y: 300, x: '100vw' }} />
     </Wrapper>
 
   )
