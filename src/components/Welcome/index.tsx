8b2d9bed193185cf141d3729288cd86bf6b621f6
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-26 06:57:53 +0500

    !fix: fixed the home page styling

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -1,16 +1,22 @@
 import { FC, memo } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, MainContainer, Text } from './index.styled';
+import {
+  Button, MainContainer, Text, Wrapper,
+} from './index.styled';
 
 const WelcomeComponent: FC<{}> = memo(() => (
+
   <MainContainer>
-    <Text>Welcome Earthlings!</Text>
-    <Link to="/loop">
-      <Button>Webworker</Button>
-    </Link>
-    <Link to="/filter">
-      <Button>Filter with Webpack</Button>
-    </Link>
+    <Wrapper>
+      <Text>Welcome Earthlings!</Text>
+      <Link to="/loop">
+        <Button>Webworker</Button>
+      </Link>
+      <Link to="/filter">
+        <Button>Filter with Webworker</Button>
+      </Link>
+    </Wrapper>
   </MainContainer>
+
 ))
 export default WelcomeComponent;
